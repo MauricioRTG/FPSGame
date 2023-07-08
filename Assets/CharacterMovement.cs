38a841066d1254e93a7020090315972254179aaa
MauricioTenorioPolyworks <mtenerio@polyworksmexico.com>
2023-07-07 19:24:49 -0600

    Add level prototype assests, coliseum shape prototype, character sprint

--- Assets/CharacterMovement.cs
+++ Assets/CharacterMovement.cs
@@ -6,10 +6,13 @@ using UnityEngine;
 
 public class CharacterMovement : MonoBehaviour
 {
-    [SerializeField] float speed = 5;
+    [SerializeField] float defaultSpeed = 5;
+    [SerializeField] float speed;
+    [SerializeField] float accelerationFactor = 1.5f;
     [SerializeField] float jumpPower = 5;
     [SerializeField] Rigidbody rigidBody;
     [SerializeField] CapsuleCollider col;
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -17,16 +20,21 @@ public class CharacterMovement : MonoBehaviour
 
         rigidBody = GetComponent<Rigidbody>();
         col = GetComponent<CapsuleCollider>();
+        speed = defaultSpeed;
     }
 
     // Update is called once per frame
     void Update()
     {
+        Sprint();
+
+        //Move
         float Horizontal = Input.GetAxis("Horizontal") * speed * Time.deltaTime;
         float Vertical = Input.GetAxis("Vertical") * speed * Time.deltaTime;
 
         transform.Translate(Horizontal, 0, Vertical);
 
+        //Jump
         if (isGrounded() && Input.GetButtonDown("Jump"))
         {
             //Add upward force to the rigid body when we press jump.
@@ -37,5 +45,20 @@ public class CharacterMovement : MonoBehaviour
     private bool isGrounded()
     {
         return Physics.Raycast(transform.position, Vector3.down, col.bounds.extents.y + 0.1f);
+    }
+
+    //Shift key is held down, increase player speed
+    //Shift key is released return to default speed
+    private void Sprint()
+    {
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            speed = defaultSpeed * accelerationFactor;
+        }
+        else if (Input.GetKeyUp(KeyCode.LeftShift))
+        {
+            speed = defaultSpeed;
+        }
     }
 }
